de2ab282b0c3bfca6d353916491169294df5a465
Mr.Stijket <mr.stijket@gmail.com>
2023-08-19 10:39:02 +0300

    Cart and Products page work together well

--- cart.js
+++ cart.js
@@ -1,16 +1,28 @@
-//const {cart} = require('./products');
+import {cart} from './products.js';
 
 const cartItemsElement = document.getElementById('cart-items');
 const cartTotalElement = document.getElementById('cart-total');
 const checkoutButton = document.getElementById('checkout-button');
 const cartModal = document.getElementById('cart-modal');
 
+
+// Retrieve cart data from localStorage when the page loads
+window.addEventListener('load', () => {
+  const savedCart = localStorage.getItem('cart');
+  if (savedCart) {
+    const parsedCart = JSON.parse(savedCart);
+    cart.length = 0; // Clear the existing cart array
+    cart.push(...parsedCart); // Push items from parsedCart to cart
+    updateCartDisplay();
+  }
+});
+
 // Update the cart display
 function updateCartDisplay() {
   cartItemsElement.innerHTML = '';
   let total = 0;
 
-  products.cart.forEach(item => {
+  cart.forEach(item => {
     const cartItem = document.createElement('li');
     cartItem.innerText = `${item.name} - $${item.price.toFixed(2)}`;
     cartItemsElement.appendChild(cartItem);
--- products.js
+++ products.js
@@ -1,7 +1,5 @@
 const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 
-const cart = [];
-
 const products = [
   { id: 1, name: 'Smartphone XYZ', price: 599.99 },
   { id: 2, name: 'Smartphone ZZZ', price: 199.99 },
@@ -12,6 +10,18 @@ const products = [
   // Add more products as needed
 ];
 
+const cart = [];
+
+// Retrieve cart data from localStorage when the page loads
+window.addEventListener('load', () => {
+  const savedCart = localStorage.getItem('cart');
+  if (savedCart) {
+    const parsedCart = JSON.parse(savedCart);
+    cart.length = 0; // Clear the existing cart array
+    cart.push(...parsedCart); // Push items from parsedCart to cart
+  }
+});
+
 // Handle "Add to Cart" button click
 addToCartButtons.forEach(button => {
   button.addEventListener('click', event => {
@@ -20,10 +30,11 @@ addToCartButtons.forEach(button => {
 
     if (productToAdd) {
       cart.push(productToAdd);
+      localStorage.setItem('cart',JSON.stringify(cart));
       alert(`${productToAdd.name} added to your cart!`);
+      console.log(cart);
     }
   });
 });
 
-
-//module.exports = {cart};
+export {products, cart};
